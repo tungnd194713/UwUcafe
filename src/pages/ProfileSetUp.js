303ef78c86b75907956c4520f8dc609fb5d48042
Anh_Tu Tran <anhtutran885@gmail.com>
2023-07-04 20:55:25 +0700

    profile setup

--- src/pages/ProfileSetUp.js
+++ src/pages/ProfileSetUp.js
@@ -7,16 +7,16 @@ const InputField = ({ label, type, value, onChange, placeholder, name }) => {
         <div className='setup-input-item'>
             <label className='setup-input-label'>{label}:
             </label>
-            <input type={type} value={value} onChange={onChange} required className='login-input-field' placeholder={placeholder} name={name} />
+            <input type={type} value={value} onChange={onChange} required className='setup-input-field' placeholder={placeholder} name={name} />
         </div>
     );
 };
 
-const RadioField = ({ label, value, checked, onChange }) => {
+const RadioField = ({ label, value, checked, onChange, name }) => {
     return (
         <div >
             <label className='setup-radio-item'>
-                <input type="radio" value={value} checked={checked} onChange={onChange} />
+                <input type="radio" value={value} checked={checked} onChange={onChange} name={name} />
                 {label}
             </label>
         </div>
@@ -43,6 +43,13 @@ function ProfileSetUp() {
     const onChangeText = (event) => {
         setValues({ ...values, [event.target.name]: event.target.value });
     }
+    const onChangeGender = (event) => {
+        const { checked, value } = event.target
+        if (checked) {
+            values.gender = value;
+            setValues({ ...values })
+        }
+    }
 
     async function onSubmit(event) {
         event.preventDefault();
@@ -53,7 +60,7 @@ function ProfileSetUp() {
                 }
             })
         }
-        catch {
+        catch (error) {
             console.error('Setup error:', error.response);
         }
     }
@@ -62,9 +69,18 @@ function ProfileSetUp() {
         <div className='res_detail_outer_container' >
             <div className='setup-container'>
                 <h1>{t("setup.header_text")}</h1>
-                <form onSubmit={onSubmit}>
+                <form onSubmit={onSubmit} style={{ width: '800px' }}>
                     <InputField name='name' onChange={onChangeText} label={t('setup.name')} />
-                    <button type='submit' className='setup-submit-btn'>Luu</button>
+                    <InputField name='email' onChange={onChangeText} label={t('setup.email')} />
+                    <InputField name='address' onChange={onChangeText} label={t('setup.address')} />
+                    <InputField name='phone_number' onChange={onChangeText} label={t('setup.phone_number')} />
+                    <div className='setup-gender-container'>
+                        <span>{t('setup.gender')}</span>
+                        <RadioField label='Nam' onChange={onChangeGender} value={1} name='gender' />
+                        <RadioField label='Nữ' onChange={onChangeGender} value={2} name='gender' />
+                    </div>
+                    <InputField name='birthday' onChange={onChangeText} label={t('setup.birthday')} placeholder='DD-MM-YYYY' />
+                    <button type='submit' className='setup-submit-btn'>Lưu</button>
                 </form>
             </div>
         </div>
